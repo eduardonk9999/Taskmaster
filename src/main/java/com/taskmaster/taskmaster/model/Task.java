4a018e2211974267f4a1ce6a70243cbd5b84d4c5
eduardonk9999 <thiago.novak7@gmail.com>
2024-10-04 20:05:22 -0300

    feat: model

--- src/main/java/com/taskmaster/taskmaster/model/Task.java
+++ src/main/java/com/taskmaster/taskmaster/model/Task.java
@@ -1,4 +1,20 @@
 package com.taskmaster.taskmaster.model;
 
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "task")
 public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+
+
+
 }
