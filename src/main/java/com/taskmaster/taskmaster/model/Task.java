77becfb9de0a7ad35af5331dbe49b219acde5b6f
eduardonk9999 <thiago.novak7@gmail.com>
2024-10-04 20:09:12 -0300

    feat: model refector

--- src/main/java/com/taskmaster/taskmaster/model/Task.java
+++ src/main/java/com/taskmaster/taskmaster/model/Task.java
@@ -14,7 +14,51 @@ public class Task {
     @Column(name = "title", nullable = false)
     private String title;
 
+    @Column(name = "description")
+    private String description;
 
+    @Column(name = "completed", nullable = false)
+    private boolean completed;
 
+    public Task(){
 
+    }
+
+    public Task(String title, String description, boolean completed) {
+        this.title = title;
+        this.description = description;
+        this.completed = completed;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
 }
